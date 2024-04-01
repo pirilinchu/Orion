61db61b1129d412678018486a9ed24dc7980ca8e
Santiago Mendoza <santiagomendozaa6@gmail.com>
2024-04-01 14:07:33 -0400

    Initial Commit

---
+++ Orion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orion
+//
+//  Created by Santiago Mendoza on 1/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orion/OrionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrionApp.swift
+//  Orion
+//
+//  Created by Santiago Mendoza on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct OrionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
